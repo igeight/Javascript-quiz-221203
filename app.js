d7750a7e6b4d2d7dd07c5e6404cd80c93f6c5990
yushi <ig88xxxxxxxxx@gmail.com>
2022-12-04 20:10:33 +0900

    bottonをクリックしてcorrectを一致したら正解を出力

--- app.js
+++ app.js
@@ -8,3 +8,12 @@ document.getElementsByTagName("button")[0].textContent = answers[0];
 document.getElementsByTagName("button")[1].textContent = answers[1];
 document.getElementsByTagName("button")[2].textContent = answers[2];
 document.getElementsByTagName("button")[3].textContent = answers[3];
+
+// buttonがクリックしたら判定
+document.getElementsByTagName("button")[0].addEventListener("click", () => {
+  if (correct === document.getElementsByTagName("button")[0].textContent) {
+    window.alert("正解！");
+  } else {
+    window.alert("不正解！");
+  }
+});

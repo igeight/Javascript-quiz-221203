dcd58ae44db663f8914fd21b38bb139916a0e9cd
yushi <ig88xxxxxxxxx@gmail.com>
2022-12-04 20:16:23 +0900

    HTMLオブジェクトを変数に入れるときは

--- app.js
+++ app.js
@@ -4,14 +4,16 @@ const correct = "ニンテンドーDS";
 
 document.getElementById("js-question").textContent = question;
 
-document.getElementsByTagName("button")[0].textContent = answers[0];
-document.getElementsByTagName("button")[1].textContent = answers[1];
-document.getElementsByTagName("button")[2].textContent = answers[2];
-document.getElementsByTagName("button")[3].textContent = answers[3];
+const $button = document.getElementsByTagName("button");
+
+$button[0].textContent = answers[0];
+$button[1].textContent = answers[1];
+$button[2].textContent = answers[2];
+$button[3].textContent = answers[3];
 
 // buttonがクリックしたら判定
-document.getElementsByTagName("button")[0].addEventListener("click", () => {
-  if (correct === document.getElementsByTagName("button")[0].textContent) {
+$button[0].addEventListener("click", () => {
+  if (correct === $button[0].textContent) {
     window.alert("正解！");
   } else {
     window.alert("不正解！");
